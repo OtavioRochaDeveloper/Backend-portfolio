a11b220bc365f853e4b128dcffb77218afc9e244
OtavioRochaDeveloper <otaviorocha36@gmail.com>
2026-01-28 08:28:00 -0300

    fix: Atualização das configurações do transporter

--- app.js
+++ app.js
@@ -8,11 +8,17 @@ app.use(cors());
 app.use(express.json());
 
 const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
   service: 'gmail',
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  tls: {
+    rejectUnauthorized: false
+  }
 })
 
 app.post('/send-email', async (req, res) => {
